577cf324b3f5e7bcb23a199424c0bf0c3a83cf57
Nihal Alfred <31736352+nihalalfred@users.noreply.github.com>
2026-04-19 17:17:26 +0100

    Create exercise1.js

---
+++ exercises/exercise1.js
@@ -0,0 +1,17 @@
+/* 
+Exercise 1: From 1 to 10, 
+Print the common multiple values of 2 & 5 
+*/
+
+// Hint: Use Remainder (Modulo) Operator: %.
+
+// it doesn't give you the result of the division; 
+// it gives you the remainder left over after dividing
+
+for(let i = 1; i<=10 ; i++ ) {
+    if(i%2==0 && i%5==0){
+
+    console.log("COmm mul of 2 & 5"+i)
+    }
+
+}
