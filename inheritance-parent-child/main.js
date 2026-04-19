09aedde946355334aa0f2656ca61ebd295062ab4
Nihal Alfred <31736352+nihalalfred@users.noreply.github.com>
2026-04-19 17:27:21 +0100

    Create main.js

---
+++ inheritance-parent-child/main.js
@@ -0,0 +1,10 @@
+// Import the Student Class
+import Student from "./student.js";
+
+// Create an object of student class
+const student = new Student("Nihal", 45, "First");
+
+// call the method to get the student details
+console.log(student.getStudentDetails());
+
+// output: Name: Nihal, Age: 45, Grade: First
